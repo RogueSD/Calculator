e961f0537e58d8466efdc2f5518d8d436d380012
ghosttamet <ghosttamet@yandex.ru>
2021-12-20 21:48:19 +0300

    минус , разделить

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public double delenie(double A, double B) {
+            
+            return A/B;
+
+        }
+
+        public double minus(double A, double B)
+        {
+            return A - B;
+        }
+    }
+}
